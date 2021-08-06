31bd819e81b39acd3089810ca702fe5a7210b46f
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-06 23:49:11 +0200

    Fixed int reader.

--- Sources/Swifties/Pos.swift
+++ Sources/Swifties/Pos.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-public struct Pos {
+public struct Pos: Equatable {
+    public static func == (lhs: Pos, rhs: Pos) -> Bool {
+        lhs._source == rhs._source && lhs._line == rhs._line && lhs._column == rhs._column
+    }
+    
     public var source: String { _source }
 
     public init(_ source: String, line: Int = 0, column: Int = 0) {
--- Sources/Swifties/readers/IntReader.swift
+++ Sources/Swifties/readers/IntReader.swift
@@ -5,7 +5,8 @@ public let intReader = IntReader()
 public class IntReader: Reader {
     @discardableResult
     public func readForm(_ p: Parser) throws -> Form? {
-        var (v, m) = (0, 1)
+        let fpos = p.pos
+        var v = 0
         
         while let c = p.getc() {
             if !c.isNumber {
@@ -18,6 +19,6 @@ public class IntReader: Reader {
             p.nextColumn()
         }
         
-        return (m == 1) ? nil : LiteralForm(env: p.env, pos: p.pos, p.env.coreLib!.intType, v)
+        return (p.pos == fpos) ? nil : LiteralForm(env: p.env, pos: p.pos, p.env.coreLib!.intType, v)
     }
 }
