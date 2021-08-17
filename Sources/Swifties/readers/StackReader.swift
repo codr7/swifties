d39054c047d75b1ceef7b818200f98394b9d674e
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-17 14:07:30 +0200

    Moved stack reader.

---
+++ Sources/Swifties/readers/StackReader.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public func stackReader(_ start: Character, _ stop: Character) -> Reader {
+    {p in
+        let fpos = p.pos
+        var c = p.getc()
+        
+        if c != start {
+            if c != nil { p.ungetc(c!) }
+            return nil
+        }
+        
+        p.nextColumn()
+        var items: [Form] = []
+
+        while true {
+            try spaceReader(p)
+            c = p.getc()
+            if c == nil || c == "]" { break }
+            p.ungetc(c!)
+            if !(try p.readForm()) { break }
+            items.append(p.popForm()!)
+        }
+
+        if c != stop { throw ReadError(p.pos, "Open stack form: \((c == nil) ? "nil" : String(c!))") }
+        p.nextColumn()
+    
+        return StackForm(env: p.env, pos: fpos, items: items)
+    }
+}
