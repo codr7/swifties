e4072d22679cffd49318586bed1f59d34340d28a
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-20 03:04:33 +0200

    Iter dump.

--- Sources/Swifties/types/IterType.swift
+++ Sources/Swifties/types/IterType.swift
@@ -3,7 +3,12 @@ import Foundation
 open class IterType: Type<Iter> {
     public override init(_ env: Env, pos: Pos, name: String, parentTypes: [AnyType]) {
         super.init(env, pos: pos, name: name, parentTypes: parentTypes)
-        dumpValue = {v in "Iter(\(v))"}
+        
+        dumpValue = {v in
+            var c = v
+            return withUnsafePointer(to: &c) {p in "Iter(\(p))"}
+        }
+        
         iterValue = {v in v as! Iter}
     }
 }
