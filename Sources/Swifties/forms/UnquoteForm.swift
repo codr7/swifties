5a05fe23be6d5d2ad3bbc8364afc44a346710cd1
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-21 07:40:30 +0200

    Fixed unquote.

--- Sources/Swifties/forms/UnquoteForm.swift
+++ Sources/Swifties/forms/UnquoteForm.swift
@@ -18,18 +18,14 @@ open class UnquoteForm: Form {
         throw EmitError(pos, "Unquote outside of quoted context")
     }
     
-    open override func quote() throws -> Slot {
-        try env.eval(_startPc)
-        return try env.pop(pos: pos)
-    }
-    
     open override func unquote() throws -> Form {
         let skipPc = env.emit(STOP)
         _startPc = env.pc
         try _form.emit()
-        env.emit(Goto(pc: env.pc), pc: skipPc)
         env.emit(STOP)
-        return self
+        env.emit(Goto(pc: env.pc), pc: skipPc)
+        try env.eval(_startPc)
+        return LiteralForm(env: env, pos: pos, try env.pop(pos: pos))
     }
 
     private let _form: Form
