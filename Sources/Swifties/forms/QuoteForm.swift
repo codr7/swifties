f065517c55d09df2dc787ea166cc57f4157ab1ad
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-08-21 02:55:00 +0200

    Added quote form.

---
+++ Sources/Swifties/forms/QuoteForm.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+open class QuoteForm: Form {
+    public init(env: Env, pos: Pos, form: Form) {
+        _form = form
+        super.init(env: env, pos: pos)
+    }
+    
+    open override func dump() -> String { "'\(_form.dump())" }
+
+    open override func expand() throws -> Form {
+        let newForm = try _form.expand()
+        if newForm != _form { return try QuoteForm(env: env, pos: pos, form: newForm).expand() }
+        return self
+    }
+
+    open override func emit() throws {
+        env.emit(Quote(env: env, pc: env.pc, form: _form))
+    }
+    
+    private let _form: Form
+}
---
+++ Sources/Swifties/ops/Quote.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+open class Quote: Op {
+    public init(env: Env, pc: Pc, form: Form) {
+        _env = env
+        _pc = pc
+        _form = form
+    }
+
+    open func eval() throws -> Pc {
+        _env.push(try _form.quote())
+        return _pc+1
+    }
+    
+    private let _env: Env
+    private let _pc: Pc
+    private let _form: Form
+}
